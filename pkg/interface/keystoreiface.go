3b6eebd52efc5510c6e659ffb66e3c2f664d29fb
cmcater <cmcater@gmail.com>
2022-04-26 14:11:51 -0400

    change iface to use keyname

--- pkg/interface/keystoreiface.go
+++ pkg/interface/keystoreiface.go
@@ -6,8 +6,15 @@ type KeyPair struct {
 }
 
 type CyptoIface interface {
+	CreateKeyPairWithKeyName(keyname string) (pubkey string, err error)
+	RemoveKeyPairByKeyName(keyname string) (err error)
+	GetKeyByKeyName(keyname string) (keypair KeyPair, err error)
+	SignByKeyName(data []byte, keyname string) (signature []byte, err error)
+	VeifyByKeyName(data []byte, keyname string) (verified bool, err error)
+
 	CreateKeyPair() (key KeyPair, err error)
-	ImportKey(privateKey string) (publicKey string, err error)
+	ImportKey(privateKey string, keyname string) (publicKey string, err error)
+	RemoveKey(publicKey string) (err error)
 	ListAll() (keys []KeyPair, err error)
 	Sign(data []byte, pubKey string) (signature []byte, err error)
 	Verify(data []byte, pubkey string) (verified bool, err error)
